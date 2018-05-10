8faf7ed54a3da9b1a1a86be7e4dd6eb920ddb4ee
Janko <jasofalcon@gmail.com>
2018-05-10 13:56:58 +0200

    stylize result page

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
 
+import "./Result.css";
 export class Result extends Component {
   render() {
     const { selectedAnswer, city } = this.props;
     return (
-      <div>
+      <div className="result">
         {selectedAnswer === city
           ? "You are absolutely correct!"
           : "You are terribly wrong!"}
